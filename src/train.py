e59be265173821bee9cdd1efc17010d0f9bf8359
Marcin Chudy <marcinchudy94@gmail.com>
2017-11-25 17:04:03 +0100

    Add tools provided by SpaceNet as submodules

---
+++ src/train.py
@@ -0,0 +1,7 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import tensorflow as tf
+
+#x8band = tf.placeholder(tf.float32, shape=[None, FLAGS.ws, FLAGS.ws, 8])
+#x3band = tf.placeholder(tf.float32, shape=[None, scale * FLAGS.ws, scale * FLAGS.ws, 3])
